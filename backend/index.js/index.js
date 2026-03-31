403e512a853d9cb719e89b3cff561eba053b5a70
kartik <kartikkumar240922@gmail.com>
2026-03-31 19:17:08 +0530

    day 1 of building swapify

---
+++ backend/index.js/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend updated successfully!");
+});
+
+const users = [];
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        skillsOffered: req.body.skillsOffered,
+        skillsWanted: req.body.skillsWanted
+    };
+
+    users.push(newUser);
+
+    res.json({
+        message: "User added successfully 🚀",
+        user: newUser
+    });
+});
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
+app.get("/match/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+
+    const currentUser = users.find(user => user.id === userId);
+
+    if (!currentUser) {
+        return res.json({ message: "User not found" });
+    }
+
+    const matches = users.filter(user => {
+        if (user.id === userId) return false;
+
+        const canTeach = user.skillsOffered.some(skill =>
+            currentUser.skillsWanted.includes(skill)
+        );
+
+        const canLearn = user.skillsWanted.some(skill =>
+            currentUser.skillsOffered.includes(skill)
+        );
+
+        return canTeach && canLearn;
+    });
+
+    res.json(matches);
+});
